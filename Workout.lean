a3fd84d04dde6e63741e18d736ce46662658ffac
iliayar <iliayar3@gmail.com>
2026-03-03 22:17:36 +0300

    feat: restore other

--- Workout.lean
+++ Workout.lean
@@ -1,10 +1,10 @@
 -- This module serves as the root of the `Workout` library.
 -- Import modules here that should be built as part of the library.
--- import Workout.Basic
--- import Workout.Logic
--- import Workout.Playground
--- import Workout.Fp
--- import Workout.Complexity.Basic
---
--- import Workout.Tapl.UntypedArithExpr
+import Workout.Basic
+import Workout.Logic
+import Workout.Playground
+import Workout.Fp
+import Workout.Complexity.Basic
+
+import Workout.Tapl.UntypedArithExpr
 import Workout.AVL
